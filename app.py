eaa7490f820c8e11c65899fa92d3fd79daa0c88c
Елизавета Артемченко <lizaartemchenko@gmail.com>
2025-10-02 23:56:11 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ def not_found(err):
     return "нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
-                <a href="/author">author</a>
+                <a href="/lab1/author">author</a>
             </body" 
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Артемченко Елизавета Сергеевна"
     group = "ФБИ-33"
@@ -32,7 +32,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
@@ -53,7 +53,7 @@ def image():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -74,15 +74,15 @@ def counter():
         </body>
     </html>
     '''
-@app.route('/reset_counter')
+@app.route('/lab1/reset_counter')
 def reset_counter():
     global count 
     count = 0
-    return redirect("/counter")
-    
-@app.route('/info')
+    return redirect("/lab1/counter")
+
+@app.route('/lab1/info ')
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route('/lab1/created')
 def created():
