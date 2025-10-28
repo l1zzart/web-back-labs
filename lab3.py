973b06f330e3d9d987ce3e21a1caf1d837c7ae3f
Елизавета Артемченко <lizaartemchenko@gmail.com>
2025-10-28 21:27:56 +0700

    Поиск товаров

--- lab3.py
+++ lab3.py
@@ -171,3 +171,83 @@ def clear():
     response.delete_cookie('font_size')
     response.delete_cookie('font_style')
     return response
+
+
+@lab3.route('/lab3/products')
+def products():
+
+    products_list = [
+        {'name': 'Porsche 911 Carrera', 'price': 12500000, 'brand': 'Porsche', 'year': 2023, 'horsepower': 385},
+        {'name': 'Chevrolet Corvette Stingray', 'price': 8500000, 'brand': 'Chevrolet', 'year': 2024, 'horsepower': 495},
+        {'name': 'BMW M4 Competition', 'price': 9200000, 'brand': 'BMW', 'year': 2023, 'horsepower': 510},
+        {'name': 'Audi R8', 'price': 15800000, 'brand': 'Audi', 'year': 2023, 'horsepower': 562},
+        {'name': 'Mercedes-AMG GT', 'price': 13200000, 'brand': 'Mercedes-Benz', 'year': 2024, 'horsepower': 523},
+        {'name': 'Nissan GT-R', 'price': 11500000, 'brand': 'Nissan', 'year': 2023, 'horsepower': 565},
+        {'name': 'Ford Mustang Shelby GT500', 'price': 7800000, 'brand': 'Ford', 'year': 2024, 'horsepower': 760},
+        {'name': 'Lamborghini Huracan', 'price': 28500000, 'brand': 'Lamborghini', 'year': 2023, 'horsepower': 640},
+        {'name': 'Ferrari F8 Tributo', 'price': 32500000, 'brand': 'Ferrari', 'year': 2023, 'horsepower': 720},
+        {'name': 'McLaren 720S', 'price': 29500000, 'brand': 'McLaren', 'year': 2024, 'horsepower': 720},
+        {'name': 'Toyota GR Supra', 'price': 5200000, 'brand': 'Toyota', 'year': 2023, 'horsepower': 382},
+        {'name': 'Honda Civic Type R', 'price': 4800000, 'brand': 'Honda', 'year': 2024, 'horsepower': 315},
+        {'name': 'Subaru WRX STI', 'price': 4500000, 'brand': 'Subaru', 'year': 2023, 'horsepower': 310},
+        {'name': 'Mazda MX-5 Miata', 'price': 3500000, 'brand': 'Mazda', 'year': 2024, 'horsepower': 181},
+        {'name': 'Porsche 718 Cayman', 'price': 6800000, 'brand': 'Porsche', 'year': 2023, 'horsepower': 300},
+        {'name': 'Aston Martin Vantage', 'price': 18500000, 'brand': 'Aston Martin', 'year': 2024, 'horsepower': 510},
+        {'name': 'Lexus LC 500', 'price': 9500000, 'brand': 'Lexus', 'year': 2023, 'horsepower': 471},
+        {'name': 'Jaguar F-Type', 'price': 8200000, 'brand': 'Jaguar', 'year': 2024, 'horsepower': 450},
+        {'name': 'Alfa Romeo Giulia Quadrifoglio', 'price': 8900000, 'brand': 'Alfa Romeo', 'year': 2023, 'horsepower': 505},
+        {'name': 'Dodge Challenger SRT Hellcat', 'price': 7200000, 'brand': 'Dodge', 'year': 2024, 'horsepower': 717}
+    ]
+
+    min_price_cookie = request.cookies.get('min_price', '')
+    max_price_cookie = request.cookies.get('max_price', '')
+    
+    min_price = request.args.get('min_price', min_price_cookie)
+    max_price = request.args.get('max_price', max_price_cookie)
+    
+    if 'reset' in request.args:
+        min_price = ''
+        max_price = ''
+    
+    all_prices = [p['price'] for p in products_list]
+    min_all_price = min(all_prices)
+    max_all_price = max(all_prices)
+    
+    filtered_products = products_list
+    
+    if min_price or max_price:
+        try:
+            min_val = float(min_price) if min_price else 0
+            max_val = float(max_price) if max_price else float('inf')
+            
+            if min_val > max_val:
+                min_val, max_val = max_val, min_val
+                min_price, max_price = str(max_val), str(min_val)
+            
+            filtered_products = [
+                p for p in products_list 
+                if min_val <= p['price'] <= max_val
+            ]
+            
+        except ValueError:
+            filtered_products = products_list
+    
+    response = make_response(render_template(
+        'lab3/products.html',
+        products=filtered_products,
+        min_price=min_price,
+        max_price=max_price,
+        min_all_price=min_all_price,
+        max_all_price=max_all_price,
+        products_count=len(filtered_products),
+        total_count=len(products_list)
+    ))
+    
+    if 'reset' not in request.args:
+        response.set_cookie('min_price', min_price, max_age=30*24*60*60)
+        response.set_cookie('max_price', max_price, max_age=30*24*60*60)
+    else:
+        response.delete_cookie('min_price')
+        response.delete_cookie('max_price')
+    
+    return response
