5af3874bfb9234aad988766c7bfde4c10d3a7555
Елизавета Артемченко <lizaartemchenko@gmail.com>
2025-11-04 20:56:39 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, redirect
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 import datetime
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 def start():
@@ -22,6 +24,7 @@ def start():
                     <li><a href='/lab1/'>Лабораторная работа 1</a></li>
                     <li><a href='/lab2/'>Лабораторная работа 2</a></li>
                     <li><a href='/lab3/'>Лабораторная работа 3</a></li>
+                    <li><a href='/lab4/'>Лабораторная работа 4</a></li>
                 </ul>
                 <hr>
                 <footer>
