de6a2a8ef0377feded07a4bb3d694a86cd60ed7e
Елизавета Артемченко <lizaartemchenko@gmail.com>
2025-11-18 00:10:14 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 import datetime
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route("/")
 def start():
@@ -28,6 +30,7 @@ def start():
                     <li><a href='/lab2/'>Лабораторная работа 2</a></li>
                     <li><a href='/lab3/'>Лабораторная работа 3</a></li>
                     <li><a href='/lab4/'>Лабораторная работа 4</a></li>
+                    <li><a href='/lab5/'>Лабораторная работа 5</a></li>
                 </ul>
                 <hr>
                 <footer>
