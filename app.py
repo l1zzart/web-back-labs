babe267b4613223f9915ddea213f5376d0339ab4
Елизавета Артемченко <lizaartemchenko@gmail.com>
2025-12-01 22:44:37 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
+
 import datetime
 import os
 
@@ -17,6 +19,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route("/")
 def start():
@@ -33,6 +36,7 @@ def start():
                     <li><a href='/lab3/'>Лабораторная работа 3</a></li>
                     <li><a href='/lab4/'>Лабораторная работа 4</a></li>
                     <li><a href='/lab5/'>Лабораторная работа 5</a></li>
+                    <li><a href='/lab6/'>Лабораторная работа 6</a></li>
                 </ul>
                 <hr>
                 <footer>
